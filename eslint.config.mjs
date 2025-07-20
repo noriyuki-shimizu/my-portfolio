b2b220d58431cdc941385729172c0dc21d25ee75
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2025-07-20 18:37:15 +0900

    Nuxt v4 アップグレード

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,22 @@
-// @ts-check
-import gitignore from 'eslint-config-flat-gitignore'
-// @ts-ignore
 import eslintConfigPrettier from 'eslint-config-prettier'
+import jsdocPlugin from 'eslint-plugin-jsdoc'
 import eslintPluginUnusedImports from 'eslint-plugin-unused-imports'
-import globals from 'globals'
 import { withNuxt } from './.nuxt/eslint.config.mjs'
 
+/** Vue files */
+const VUE_FILES = ['**/*.{jsx,tsx,vue}']
+
+/** All files */
+const ALL_FILES = ['**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts,vue}']
+
+/** TypeScript files that require type information */
+const TS_FILES = ['**/*.{ts,tsx,cts,mts,vue}']
+
 export default withNuxt([
-  gitignore(),
-  eslintConfigPrettier,
+  /* Vue におけるルールセット */
   {
-    files: ['**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts,vue}'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    },
-    plugins: {
-      'unused-imports': eslintPluginUnusedImports
-    },
+    files: VUE_FILES,
     rules: {
-      /* eslint */
       'no-restricted-properties': [
         'error',
         {
@@ -33,6 +28,42 @@ export default withNuxt([
           message: '廃止されたメソッドです'
         }
       ],
+      'vue/component-definition-name-casing': ['error', 'PascalCase'],
+      'vue/component-name-in-template-casing': ['error', 'PascalCase', { registeredComponentsOnly: false, ignores: [] }]
+    }
+  },
+  /* TypeScript で型情報が必要なルールセット */
+  {
+    files: TS_FILES,
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    rules: {
+      '@typescript-eslint/strict-boolean-expressions': [
+        'warn',
+        {
+          allowString: false,
+          allowNumber: false,
+          allowNullableObject: false,
+          allowNullableBoolean: false,
+          allowNullableString: false,
+          allowNullableNumber: false,
+          allowAny: false
+        }
+      ]
+    }
+  },
+  /* 全体的なルールセット */
+  {
+    files: ALL_FILES,
+    plugins: {
+      jsdoc: jsdocPlugin,
+      'unused-imports': eslintPluginUnusedImports
+    },
+    rules: {
+      /* eslint */
       'no-restricted-imports': [
         'error',
         {
@@ -48,30 +79,92 @@ export default withNuxt([
               group: ['**/*.vue'],
               allowImportNames: ['default'],
               message: 'default export を使用してください'
+            },
+            {
+              group: ['@/composables/*', '@/utils/*'],
+              message: '自動 import 対象になるので、明示的に import する必要はありません'
             }
-            // TODO: 下記ルールは ESLint の見直しの際に削除する
-            // {
-            //   group: ['@/composables/*', '@/utils/*'],
-            //   message: '自動 import 対象になるので、明示的に import する必要はありません'
-            // }
           ]
         }
       ],
+      'arrow-body-style': ['error', 'always'],
+      /* typescript-eslint */
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: false,
+          allowTypedFunctionExpressions: true,
+          allowHigherOrderFunctions: true,
+          allowDirectConstAssertionInArrowFunctions: false,
+          allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+          allowFunctionsWithoutTypeParameters: false,
+          allowIIFEs: false,
+          allowedNames: []
+        }
+      ],
+      /* JSDoc */
+      'jsdoc/require-jsdoc': [
+        'error',
+        {
+          publicOnly: false,
+          contexts: [
+            'FunctionDeclaration',
+            'MethodDefinition',
+            'ClassDeclaration',
+            'TSPropertySignature',
+            'TSTypeAliasDeclaration',
+            'TSInterfaceDeclaration',
+            'VariableDeclaration[parent.type="Program"]',
+            'ExportNamedDeclaration[declaration!=null]'
+          ]
+        }
+      ],
+      'jsdoc/require-description': ['error', { descriptionStyle: 'any' }],
+      'jsdoc/check-param-names': [
+        'error',
+        { enableFixer: true, disableMissingParamChecks: true, useDefaultObjectProperties: true }
+      ],
+      'jsdoc/empty-tags': 'error',
+      'jsdoc/no-defaults': 'error',
+      'jsdoc/no-multi-asterisks': ['error', { allowWhitespace: true }],
+      'jsdoc/require-param': ['error', { checkDestructuredRoots: false }],
+      'jsdoc/require-param-name': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/require-returns-check': 'error',
+      'jsdoc/require-returns-description': 'error',
+      'jsdoc/no-types': 'error',
       /* import */
       'import/first': 'off',
       'import/no-self-import': 'off',
+      'import/order': [
+        'error',
+        {
+          // TODO: [type] プロパティを追加するかメンバーに相談する
+          groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'internal', 'object'],
+          pathGroups: [
+            {
+              pattern: '#*',
+              group: 'external',
+              position: 'after'
+            }
+          ],
+          pathGroupsExcludedImportTypes: ['builtin', 'external', 'object'],
+          distinctGroup: false,
+          named: true,
+          alphabetize: { order: 'asc' },
+          sortTypesGroup: true
+        }
+      ],
       /* eslint-plugin-unused-imports */
-      'unused-imports/no-unused-imports': 'error',
-      // TODO: 下記の設定は enabled にしたい
-      'no-console': 'off',
-      camelcase: 'off',
-      // MEMO: 以下のルールは InjectionKey を props に渡す設計になっており、Symbol の定義が必要なため、disabled にする
-      '@typescript-eslint/no-wrapper-object-types': 'off',
-      // TODO: 下記ルールは ESLint の見直しの際に削除する
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-expressions': 'off',
-      'no-constant-binary-expression': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off'
+      'unused-imports/no-unused-imports': 'error'
+    }
+  },
+  eslintConfigPrettier,
+  /** Prettier と競合しているルールの上書き */
+  {
+    files: ALL_FILES,
+    rules: {
+      curly: ['error', 'all']
     }
   }
 ])
