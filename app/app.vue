f4fac657155fc9d9b04a9daf565dda6bb7b97a59
Shiminori <31028330+noriyuki-shimizu@users.noreply.github.com>
2026-03-13 01:10:12 +0900

    fix: package.jsonのtypecheckコマンドを修正し、依存関係のバージョンを更新 (#3)

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,7 @@
-<script setup lang="ts">
-import { Analytics } from '@vercel/analytics/nuxt'
-</script>
-
 <template>
   <div>
     <NuxtAnnouncer />
     <NuxtRouteAnnouncer />
-    <Analytics />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
